616e6dd2d7356addb5fda75aa338d4ed6c6534f5
PawnowskiDev <pawnowski@gmail.com>
2023-11-14 23:14:53 +0100

    skonfigurowano server tomcat

---
+++ src/main/java/pl/eszkola/controller/HomeController.java
@@ -0,0 +1,13 @@
+package pl.eszkola.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/eszkola")
+    public String home() {
+        return "templates/index.html";
+    }
+}
